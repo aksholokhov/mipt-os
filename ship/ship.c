da01ee40cf0d2e4f363851776c96935e824e5b38
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-10-24 21:43:31 +0300

    ship finished

--- ship/ship.c
+++ ship/ship.c
@@ -8,9 +8,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <signal.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define TRIPNUM 5
-#define PNUM 100
+
+//initial number of passengers
+#define PNUM 10
+
 #define SHIP_CAPACITY 3
 
 int main(int argc, char** argv) {
@@ -19,47 +24,59 @@ int main(int argc, char** argv) {
         perror("fail to create semaphore");
     } 
     int p_pid[PNUM];
+    //initialize shared memory for current number of people on the beach
+    int key = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | S_IRWXU);
+    int* p_num = (int*)shmat(key, NULL, 0);
+    *p_num = PNUM;
+
     int ship_pid = fork();
     if (ship_pid == 0) {
         //ship
+        p_num = (int*)shmat(key, NULL, 0);
         struct sembuf sb;
-        for (int i = 0; i < TRIPNUM; i++) {    
+        while(*p_num > 0) {    
             sb.sem_num = 0;
-            sb.sem_op = SHIP_CAPACITY;
+            //printf("PNUM = %d \n", *p_num);
+            sb.sem_op = (SHIP_CAPACITY > *p_num) ? *p_num : SHIP_CAPACITY;
             sb.sem_flg = 0;
-            printf("SHIP: Desk has opened for boarding\n");
+            printf("CAPTAIN: Desk has opened for boarding\n");
             if (semop(sem_id, &sb, 1) == -1) {
-                perror("SHIP: Can't open desk for boarding");
+                perror("CAPTAIN: Can't open desk for boarding");
                 return 1;
             }
             sb.sem_op = 0;
             if (semop(sem_id, &sb, 1) == -1) {
-                perror("SHIP: Wait fail");
+                perror("CAPTAIN: Wait fail");
                 return 1;
             }
+            //these sieeps need only for better log reading (without them log
+            //sometimes renders in a wrong order) 
             sleep(1);
-            printf("SHIP: Ship is full now! Sail away! \n");
+            printf("CAPTAIN: Ship is full now! Sail away! \n ~~~~~~~~~~~~~ \n");
             sleep(3);
-            printf("SHIP: Arrived, good bye, sailors! \n");
+            printf("CAPTAIN: Arrived, good bye, sailors! \n");
             sb.sem_num = 1;
-            sb.sem_op = SHIP_CAPACITY;
+            sb.sem_op = (SHIP_CAPACITY > *p_num) ? *p_num : SHIP_CAPACITY;
             if (semop(sem_id, &sb, 1) == -1) {
-                perror("SHIP: Can't open desk for arriving");
+                perror("CAPTAIN: Can't open desk for arriving");
                 return 1;
             }
             sb.sem_op = 0;
             if (semop(sem_id, &sb, 1) == -1) {
-                perror("SHIP: Can't wait for passengers leaving");
+                perror("CAPTAIN: Can't wait for passengers leaving");
                 return 1;
             }
             sleep(1);
         }
+        printf("CAPTAIN: We finished!\n");
+        return 0;
     }
     else {
         for (int i = 0; i < PNUM; i++) {
             int pid = fork();
             if (pid == 0) {
               //passenger
+                p_num = (int*)shmat(key, NULL, 0);
                 struct sembuf sb;
                 while (1) {
                     sb.sem_num = 0;
@@ -75,13 +92,23 @@ int main(int argc, char** argv) {
                         perror("Failed to leave");
                         return 1;
                     }
-                    printf("PASSENGER %d: left the ship \n", i);
+                    printf("PASSENGER %d: left the ship and ", i);
+                    srand(time(NULL)*(i+1));
+                    int stay = rand() % 2;
+                    if (stay) {
+                        printf("wants again! \n");
+                    } else {
+                        printf("was tired: went home.\n");
+                        (*p_num)--;
+                        return 0;
+                    }
                 }
             } else {
                 p_pid[i] = pid;
             }   
         }
     }
+
     int status;
     waitpid(ship_pid, &status, 0);
     for (int i = 0; i < PNUM; i++) {
